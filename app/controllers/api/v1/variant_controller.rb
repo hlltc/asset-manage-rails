9e2d47c08bb068238e2443cfeb020868d9012979
Tianci Liu <hlltchlltc@gmail.com>
2015-09-10 11:50:35 +0800

    add asset variant match logic
    add asset variant match logic

--- app/controllers/api/v1/variant_controller.rb
+++ app/controllers/api/v1/variant_controller.rb
@@ -1,9 +1,29 @@
 class Api::V1::VariantController < Api::V1::BaseController
   # GET /variant
   # GET /variant.json
+  # todo: when conditions not specified, query would set them to "all", instead of not specifying.
   def index
     @asset = Asset.find(params[:asset_id])
-    @variants = @asset.variants.all
+
+    @device = params[:device]? params[:device] : "all"
+    @size = params[:size]? params[:size] : "all"
+    @language = params[:language]? params[:language] : "all"
+    
+    #try to find matched items
+    @variants = @asset.variants.where(:device => @device, :size => @size, :language => @language).order("created_at DESC")
+    #if [], :language => "all"
+    if @variants.length < 1
+      @variants = @asset.variants.where(:device => @device, :size => @size, :language => "all").order("created_at DESC")
+    end
+    #if [], :language => "all", :size => "all"
+    if @variants.length < 1
+      @variants = @asset.variants.where(:device => @device, :size => "all", :language => "all").order("created_at DESC")
+    end
+    #if [], :language => "all", :size => "all", :device => "all"
+    if @variants.length < 1
+      @variants = @asset.variants.where(:device => "all", :size => "all", :language => "all").order("created_at DESC")
+    end
+
     render json: {variants: @variants.map {|variant| variant.info_to_json}}
   end
 

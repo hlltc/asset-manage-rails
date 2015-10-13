1ea79cae415691e8214497cdf6186521c2cd2485
Tianci Liu <hlltchlltc@gmail.com>
2015-10-13 10:34:45 +0800

    1. add basic auth test 2. fix test fails by adding request authorization env

--- test/controllers/asset_controller_test.rb
+++ test/controllers/asset_controller_test.rb
@@ -18,23 +18,39 @@ class AssetControllerTest < ActionController::TestCase
   end
 
   test "should create asset" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     post :create, asset: {title: "asset new", description: "description of asset new"}
     assert_response :success
     assert_not_nil assigns(:asset)
   end
+  test "should not create asset without auth" do
+    post :create, asset: {description: "description of asset new"}
+    assert_response 401
+  end
   test "should not create asset" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     post :create, asset: {description: "description of asset new"}
     assert_response 422
   end
 
   test "should put update" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     put :update, id: @asset_1.id, asset: {title: "asset new", description: "description of asset new"}
     assert_response :success
     assert_not_nil assigns(:asset)
   end
+  test "should not put update without auth" do
+    put :update, id: @asset_1.id, asset: {title: "asset new", description: "description of asset new"}
+    assert_response 401
+  end
 
   test "should delete destroy" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     delete :destroy, id: @asset_1.id
     assert_response :success
+  end
+  test "should not delete destroy without auth" do
+    delete :destroy, id: @asset_1.id
+    assert_response 401
   end
 end
--- test/controllers/variant_controller_test.rb
+++ test/controllers/variant_controller_test.rb
@@ -34,32 +34,50 @@ class VariantControllerTest < ActionController::TestCase
   end
 
   test "should create variant" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     post :create, variant: {device: "all", size: "all", language: "all", asset_id: @asset_1.id}
     assert_response :success
     assert_not_nil assigns(:variant)
   end
+  test "should not create variant without auth" do
+    post :create, variant: {device: "all", size: "all", language: "all", asset_id: @asset_1.id}
+    assert_response 401
+  end
   test "should not create variant without required field" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     post :create, variant: {description: "description of variant new"}
     assert_response 422
   end
   test "should not create variant with wrong value" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     post :create, variant: {device: "description of variant new", size: "all", language: "all", asset_id: @asset_1.id}
     assert_response 422
   end
 
   test "should put update" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     put :update, id: @variant_1.id, variant: {device: "iPad"}
     assert_response :success
     assert_not_nil assigns(:variant)
   end
+  test "should not put update without auth" do
+    put :update, id: @variant_1.id, variant: {device: "iPad"}
+    assert_response 401
+  end
   test "should not put update" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     put :update, id: @variant_1.id, variant: {device: "winPhone"}
     assert_response 422
     assert_not_nil assigns(:variant)
   end
 
   test "should delete destroy" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin')
     delete :destroy, id: @variant_1.id
     assert_response :success
+  end
+  test "should not delete destroy without auth" do
+    delete :destroy, id: @variant_1.id
+    assert_response 401
   end
 end
